ef7f88810507d16e2bf70a2bc9bf24ec10ba1012
Joshua Byrd <phocks@gmail.com>
2022-06-15 15:23:30 +1000

    Simplify!!!

---
+++ Components/App/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./App.module.scss";
+
+const App = () => {
+  return <div className={styles.root}>Data tools for Deep Time.</div>;
+};
+
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import App from "../Components/App";
 
 export default function Home() {
